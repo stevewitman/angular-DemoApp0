bf983e1ce5a516897783ac28df68c5937311e03e
Steve Witman <stevewitman@gmail.com>
2016-02-28 16:46:39 -0700

    end of Writing Custom Filters

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -17,7 +17,7 @@ eventsApp.controller('EventController',
         {
           name: 'Directives Masterclass',
           creatorName: 'Bob Smith',
-          duration: '1 hr',
+          duration: 1,
           level: 'Advanced',
           abstract: 'In this session you will learn the ins and outs of directives.',
           upVoteCount: 0
@@ -25,7 +25,7 @@ eventsApp.controller('EventController',
         {
           name: 'Scopes for fun and profit',
           creatorName: 'John Doe',
-          duration: '30 mins',
+          duration: 2,
           level: 'Introductory',
           abstract: 'This session will take a closer look at scopes. Learn what they do, how they do it, and how to get then to do it for you.',
           upVoteCount: 0
@@ -33,7 +33,7 @@ eventsApp.controller('EventController',
         {
           name: 'Well behaved Controllers',
           creatorName: 'Jane Doe',
-          duration: '2 hours',
+          duration: 4,
           level: 'Intermediate',
           abstract: 'Controllers are the beginning of everyting Angular does. Learn how to craft controllers that will win the respect of your friends and neighbors.',
           upVoteCount: 0
