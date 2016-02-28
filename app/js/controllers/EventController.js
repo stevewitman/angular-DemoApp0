bee854af2da317de6919192400ad79fe963a422e
Steve Witman <stevewitman@gmail.com>
2016-02-28 16:37:56 -0700

    end of Built-in filters

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,6 +2,7 @@
 
 eventsApp.controller('EventController',
   function EventController($scope) {
+    $scope.sortorder = 'name';
     $scope.event = {
       name: 'Angular Boot Camp',
       date: '1/1/2013',
