de98cc84165154a495a98cb636bbfa2594c68946
Steve Witman <stevewitman@gmail.com>
2016-02-28 20:45:12 -0700

    end of Validation

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,11 +2,16 @@
 
 eventsApp.controller('EditEventController',
   function EditEventController($scope) {
-    $scope.saveEvent = function(event) {
-      window.alert('event ' + event.name + ' saved!');
-    }
+
+    $scope.saveEvent = function(event, newEventForm) {
+      console.log(newEventForm);
+      if (newEventForm.$valid) {
+        window.alert('event ' + event.name + ' saved!');
+      }
+    };
+
     $scope.cancelEdit = function() {
       window.location = "/EventDetails.html"
-    }
+    };
   }
 );
