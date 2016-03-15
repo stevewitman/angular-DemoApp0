b9414dcaf12a916c16295e0b897d056d2d3abec8
Steve Witman <stevewitman@gmail.com>
2016-03-14 23:01:59 -0600

    using angular's  service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,11 +3,11 @@
 eventsApp.controller('EventController',
   function EventController($scope, eventData, $log) {
     $scope.sortorder = 'name';
-      eventData.getEvent()
-        .success(function(event) { $scope.event = event; })
-        .error(function(data, status, headers, config) {
-          $log.warn(data, status, headers, config);
-        });
+    eventData.getEvent()
+      .$promise
+        .then(function(event) { $scope.event = event; })
+        .catch(function(response) { console.log(response); }
+      );
 
     $scope.upVoteSession = function(session) {
       session.upVoteCount++;
